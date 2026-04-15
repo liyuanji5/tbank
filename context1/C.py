3595cd36eaa963b3ae8c620952d592b8cd0fefc7
liyuanji5 <liyuanji0112@gmail.com>
2026-04-15 14:59:13 +0700

    Update C.py

--- context1/C.py
+++ context1/C.py
@@ -1,1 +1,21 @@
+from sys import stdout
 
+def query(x):
+    print(x)
+    stdout.flush()
+    return input()
+
+def guess_number(n):
+    l = 1
+    r = n
+    while l < r:
+        m = (l + r + 1) // 2   
+        response = query(m)
+        if response == '<':
+            r = m - 1
+        else:  # response == '>='
+            l = m
+    print(f'! {l}')
+
+n = int(input())
+guess_number(n)
