aee5668cd1c2b53551fdd7367944a8a075844506
liyuanji5 <liyuanji0112@gmail.com>
2026-04-15 15:02:22 +0700

    Update D.py

--- context1/D.py
+++ context1/D.py
@@ -1,1 +1,15 @@
+def equation(x):
+    return x ** 2 + (x + 1) ** 0.5
 
+c = float(input())
+l = 0
+r = c ** 0.5
+
+while r - l > 1e-7:  
+    mid = (l + r) / 2
+    if equation(mid) > c:
+        r = mid
+    else:
+        l = mid
+
+print(round((l + r) / 2, 6))
